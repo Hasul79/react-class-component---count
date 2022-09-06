727ae4587521399e81335b3d0ab0e65a4d678784
Hasul79 <95657084+Hasul79@users.noreply.github.com>
2022-09-06 15:59:43 +0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,19 +3,7 @@ import "./style.css";
 import Child from './Child';
 
 export default  class App extends React.Component {
-  state = {count: 0}
-  gumar = () => {
-       this.setState({
-         count: this.state.count + 1,
-        });
-      };
-
-  hanum = () => {
-        this.setState({
-          count: this.state.count - 1,
-      });
-    };
- 
+  
 
   render() {
     const {count} = this.state;
